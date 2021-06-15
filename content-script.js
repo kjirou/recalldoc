e2c13ba7804b386c04f85124da6d0cbb7aa37648
kjirou <sorenariblog@gmail.com>
2021-06-15 22:32:48 +0900

    Save footprints data

--- content-script.js
+++ content-script.js
@@ -4,6 +4,17 @@ const getPageKind = (url) => {
   }
   return 'unknown'
 }
+const loadFootprints = () => {
+  const rawFootprints = window.localStorage.getItem('footprints')
+  if (!rawFootprints) {
+    return []
+  }
+  return JSON.parse(rawFootprints)
+}
+const saveFootprints = (footprints) => {
+  const serializedFootprints = JSON.stringify(footprints)
+  window.localStorage.setItem('footprints', serializedFootprints)
+}
 
 const pageKind = getPageKind(document.URL)
 
@@ -11,5 +22,16 @@ if (pageKind === 'note') {
   const pageTitle = document.querySelector('#title span').textContent
   // TODO: .folderIndicator の中には複数要素が含まれているので、それを現在は textContent で強引に結合している。
   const folderIndicatorLabel = document.querySelector('.folderIndicator').textContent
-  console.log(`${folderIndicatorLabel}/${pageTitle}`)
+  const footprint = {
+    title: `${folderIndicatorLabel}/${pageTitle}`,
+    // TODO: Query Stringなどを落として正規化する。
+    url: document.URL,
+  }
+  // TODO: 同じ URL を追加しない。
+  const footprints = [
+    ...loadFootprints(),
+    footprint,
+  ]
+  saveFootprints(footprints)
+  console.log(footprints)
 }
