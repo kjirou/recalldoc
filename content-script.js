2d141456997cbf5a0ae6bda9e6c4d005f75bbae1
kjirou <sorenariblog@gmail.com>
2021-06-15 22:17:17 +0900

    Pick up page and folder names from dom tree

--- content-script.js
+++ content-script.js
@@ -1,1 +1,15 @@
-console.log('Hiya, Georgie!')
+const getPageKind = (url) => {
+  if (/\/notes\/\d+(\?|$)/.test(url)) {
+    return 'note'
+  }
+  return 'unknown'
+}
+
+const pageKind = getPageKind(document.URL)
+
+if (pageKind === 'note') {
+  const pageTitle = document.querySelector('#title span').textContent
+  // TODO: .folderIndicator の中には複数要素が含まれているので、それを現在は textContent で強引に結合している。
+  const folderIndicatorLabel = document.querySelector('.folderIndicator').textContent
+  console.log(`${folderIndicatorLabel}/${pageTitle}`)
+}
