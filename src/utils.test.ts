d1abe7dc7e93d48172a835fe5eaa5ff19dc20fa1
kjirou <sorenariblog@gmail.com>
2021-06-19 17:01:56 +0900

    Split search query into multipul keywords

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,8 @@
 import {
   Footprint,
   updateFootprints,
+  searchFootprints,
+  splitSearchQueryIntoMultipulKeywords,
 } from './utils'
 
 describe('utils', () => {
@@ -49,6 +51,77 @@ describe('utils', () => {
     ]
     test.each(table)('$name', ({footprints, newFootprint, expected}) => {
       expect(updateFootprints(footprints, newFootprint)).toStrictEqual(expected)
+    })
+  })
+  describe('splitSearchQueryIntoMultipulKeywords', () => {
+    const table: {
+      expected: string[],
+      query: string,
+    }[] = [
+      {
+        query: 'foo',
+        expected: ['foo'],
+      },
+      {
+        query: '  foo  ',
+        expected: ['foo'],
+      },
+      {
+        query: '\u3000\u3000foo\u3000\u3000',
+        expected: ['foo'],
+      },
+      {
+        query: '\u3000 \u3000foo \u3000 ',
+        expected: ['foo'],
+      },
+      {
+        query: 'foo bar \u3000baz',
+        expected: ['foo', 'bar', 'baz'],
+      },
+      {
+        query: '',
+        expected: [],
+      },
+      {
+        query: ' ',
+        expected: [],
+      },
+    ]
+    test.each(table)(`"$query" -> $expected`, ({query, expected}) => {
+      expect(splitSearchQueryIntoMultipulKeywords(query)).toStrictEqual(expected)
+    })
+  })
+  describe('searchFootprints', () => {
+    const createFootprints = (...titles: Footprint['title'][]): Footprint[] => {
+      return titles.map(title => ({title, url: ''}))
+    }
+    const table: {
+      expected: Footprint[],
+      footprints: Footprint[],
+      name: string,
+      searchQuery: string,
+    }[] = [
+      {
+        name: 'It searches by partial match',
+        footprints: createFootprints('foox', 'bar', 'baz'),
+        searchQuery: 'ba',
+        expected: createFootprints('bar', 'baz'),
+      },
+      {
+        name: 'It searches by case insensitive',
+        footprints: createFootprints('abc', 'ABC'),
+        searchQuery: 'AbC',
+        expected: createFootprints('abc', 'ABC'),
+      },
+      {
+        name: 'It returns all footprints when the search query is empty',
+        footprints: createFootprints('foo', 'bar', 'baz'),
+        searchQuery: '',
+        expected: createFootprints('foo', 'bar', 'baz'),
+      },
+    ]
+    test.each(table)(`$name`, ({footprints, searchQuery, expected}) => {
+      expect(searchFootprints(footprints, searchQuery)).toStrictEqual(expected)
     })
   })
 })
