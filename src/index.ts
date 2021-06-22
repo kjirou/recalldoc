a948ce18ffcd9c84fcb421613d0fd14f6e2ce81a
kjirou <sorenariblog@gmail.com>
2021-06-22 23:27:37 +0900

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -66,6 +66,7 @@ switch (pageMataData.siteId) {
         const footprints = loadFootprints()
         saveFootprints(updateFootprints(footprints, newFootprint))
       }
+    // TODO: 左ナビからのカテゴリ画面への遷移は Ajax なので、その画面遷移経路だとここの分岐を通らず保存されない。
     } else if (pageMataData.contentKind === 'category') {
       const categoryPath = decodeURIComponent(location.hash.replace(/^#path=/, '')).replace(/^\//, '')
       if (categoryPath) {
@@ -82,19 +83,19 @@ switch (pageMataData.siteId) {
   case 'kibela': {
     prepareUi()
     if (pageMataData.contentKind === 'note') {
-      // TODO: HTML 要素がなかったときの対応をする。
-      const pageTitle = document.querySelector('#title span')!.textContent
-      // TODO: .folderIndicator の中には複数要素が含まれているので、それを現在は textContent で強引に結合している。
-      // TODO: HTML 要素がなかったときの対応をする。
-      const folderIndicatorLabel = document.querySelector('.folderIndicator')!.textContent
-      const newFootprint = {
-        title: `${folderIndicatorLabel}/${pageTitle}`,
-        url: location.origin + location.pathname,
+      const titleElement = document.querySelector('#title span')
+      // NOTE: .folderIndicator の中には複数要素が含まれていおり、それを textContent で強引に結合している。
+      const folderIndicatorElement = document.querySelector('.folderIndicator')
+      if (titleElement && folderIndicatorElement) {
+        const newFootprint: Footprint = {
+          title: `${folderIndicatorElement.textContent}/${titleElement.textContent}`,
+          url: location.origin + location.pathname,
+        }
+        // TODO: 非同期でやっても良さそう。
+        const footprints = loadFootprints()
+        saveFootprints(updateFootprints(footprints, newFootprint))
       }
-      // TODO: 非同期でやっても良さそう。
-      const footprints = loadFootprints()
-      saveFootprints(updateFootprints(footprints, newFootprint))
-    // TODO: folder の画面から他のフォルダーに遷移する上部のリンクが Ajax になっているので、その画面遷移経路だと保存できない。
+    // TODO: folder の画面から他のフォルダーに遷移する上部のリンクが Ajax になっているので、その画面遷移経路だとここの分岐を通らず保存されない。
     } else if (pageMataData.contentKind === 'folder') {
       const folder = decodeURIComponent(location.pathname.replace(/^\/notes\/folder\//, ''))
       const newFootprint: Footprint = {
