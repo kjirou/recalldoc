84f2aa52c4282945f6f9c294b2e5dfc99e54f919
kjirou <sorenariblog@gmail.com>
2021-06-23 09:49:26 +0900

    Rewrite styles with a raw style tag

--- src/components/Searcher.tsx
+++ src/components/Searcher.tsx
@@ -18,6 +18,39 @@ export type Props = {
   onMount: (searchFieldElement: HTMLInputElement) => void;
 }
 
+/**
+ * NOTE: .searcher の z-index は、esa の上部バー(nav.navbar-sub)の z-index: 3; より高くする必要がある。
+ */
+const styleLiteral = `
+  .searcher {
+    --width: 600px;
+    width: var(--width);
+    position: fixed;
+    top: 20px;
+    left: calc(50% - var(--width)/2);
+    z-index: 4;
+  }
+  input.searcher__searchQuery {
+    display: block;
+    width: 100%;
+    text-align: right;
+  }
+  ul.searcher__itemList {
+    padding: 5px;
+    border: 1px solid #ccc;
+    background-color: #fff;
+  }
+  ul.searcher__itemList > li {
+    line-height: 1;
+  }
+  ul.searcher__itemList > li.highlighted {
+    background-color: #ff0;
+  }
+  ul.searcher__itemList > li > a {
+    font-size: 12px;
+  }
+`
+
 // TODO: 最大表示件数を設定する。
 // TODO: 検索キーワードがマッチしている箇所をハイライトする。
 export const Searcher: VFC<Props> = (props) => {
@@ -29,55 +62,33 @@ export const Searcher: VFC<Props> = (props) => {
     }
   }, [props, props.onMount])
 
-  return <div
-    style={{
-      width: '600px',
-      position: 'fixed',
-      top: '20px',
-      left: 'calc(50% - 600px/2)',
-      // NOTE: esa の上部バー(nav.navbar-sub)の z-index が 3 。
-      zIndex: 4,
-    }}
-  >
-    <input
-      ref={searchFieldRef}
-      onInput={event => {
-        props.onInput(event.currentTarget.value)
-      }}
-      onKeyDown={props.onKeyDown}
-      style={{
-        display: 'block',
-        textAlign: 'right',
-        width: '100%',
-      }}
-    />
-    {
-      props.footprints.length > 0 && <ul
-        style={{
-          border: '1px solid #cccccc',
-          backgroundColor: '#ffffff',
-          padding: '5px',
+  return <>
+    <style>{styleLiteral}</style>
+    <div className="searcher">
+      <input
+        className="searcher__searchQuery"
+        ref={searchFieldRef}
+        onInput={event => {
+          props.onInput(event.currentTarget.value)
         }}
-      >
-        {
-          props.footprints.map(footprint => {
-            return <li
-              key={ footprint.url }
-              style={{
-                lineHeight: '1',
-                ...(footprint.highlighted ? { backgroundColor: '#ffff00' } : {}),
-              }}
-            >
-              <a
-                href={ footprint.url }
-                style={{
-                  fontSize: '12px',
-                }}
-              >{ footprint.title }</a>
-            </li>
-          })
-        }
-      </ul>
-    }
-  </div>
+        onKeyDown={props.onKeyDown}
+      />
+      {
+        props.footprints.length > 0 && <ul className="searcher__itemList">
+          {
+            props.footprints.map(footprint => {
+              return <li
+                key={ footprint.url }
+                className={footprint.highlighted ? 'highlighted' : undefined}
+              >
+                <a
+                  href={ footprint.url }
+                >{ footprint.title }</a>
+              </li>
+            })
+          }
+        </ul>
+      }
+    </div>
+  </>
 }
