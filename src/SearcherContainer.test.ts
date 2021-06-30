aa7e22e2eb81bdeca078220352372691423316ea
kjirou <sorenariblog@gmail.com>
2021-06-30 10:13:14 +0900

    テストファイルの一番外側のdescribeを削除
    Jest は、テストファイル単位で処理を挿入できる仕組みが揃っているので（直下のbeforeEach, afterEachがテストファイルだけに影響するなど）、特に括る必要はなさそうだった。
    --verbose の時の表示も、テストファイル名が含まれているので十分そう。

--- src/SearcherContainer.test.ts
+++ src/SearcherContainer.test.ts
@@ -11,36 +11,34 @@ import {
   Storage,
 } from './storage'
 
-describe('SearcherContainer', () => {
-  describe('useStorageSynchronization', () => {
-    const renderUseStorageSynchronization = (...args: Parameters<typeof useStorageSynchronization>) => {
-      return renderHook(
-        ({storage, footprints}) => useStorageSynchronization(storage, footprints),
-        {initialProps: {storage: args[0], footprints: args[1]}},
-      )
+describe('useStorageSynchronization', () => {
+  const renderUseStorageSynchronization = (...args: Parameters<typeof useStorageSynchronization>) => {
+    return renderHook(
+      ({storage, footprints}) => useStorageSynchronization(storage, footprints),
+      {initialProps: {storage: args[0], footprints: args[1]}},
+    )
+  }
+  let storage: Storage
+
+  beforeEach(() => {
+    storage = {
+      loadFootprints: jest.fn(),
+      saveFootprints: jest.fn(),
+      updateFootprints: jest.fn(),
     }
-    let storage: Storage
+  })
 
-    beforeEach(() => {
-      storage = {
-        loadFootprints: jest.fn(),
-        saveFootprints: jest.fn(),
-        updateFootprints: jest.fn(),
-      }
-    })
-
-    test('it does not call saveFootprints when footprints is the same', () => {
-      const footprints: Footprint[] = []
-      const {rerender} = renderUseStorageSynchronization(storage, footprints)
-      rerender({storage, footprints})
-      expect(storage.saveFootprints).not.toHaveBeenCalled()
-    })
-    test('it calls saveFootprints when footprints is different', () => {
-      const footprints: Footprint[] = []
-      const {rerender} = renderUseStorageSynchronization(storage, footprints)
-      const newFootprints: Footprint[] = [{title: '', url: ''}]
-      rerender({storage, footprints: newFootprints})
-      expect(storage.saveFootprints).toHaveBeenCalledTimes(1)
-    })
+  test('it does not call saveFootprints when footprints is the same', () => {
+    const footprints: Footprint[] = []
+    const {rerender} = renderUseStorageSynchronization(storage, footprints)
+    rerender({storage, footprints})
+    expect(storage.saveFootprints).not.toHaveBeenCalled()
+  })
+  test('it calls saveFootprints when footprints is different', () => {
+    const footprints: Footprint[] = []
+    const {rerender} = renderUseStorageSynchronization(storage, footprints)
+    const newFootprints: Footprint[] = [{title: '', url: ''}]
+    rerender({storage, footprints: newFootprints})
+    expect(storage.saveFootprints).toHaveBeenCalledTimes(1)
   })
 })
