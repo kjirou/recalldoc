8e0c7078d1874a942441c069c2a48c76ab6dc47f
kjirou <sorenariblog@gmail.com>
2021-07-22 22:21:55 +0900

    ライセンス情報を.jsへ含める

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const productionMode = process.env.NODE_ENV === 'production'
+
+const optimization = productionMode
+  ? {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  }
+  : {}
 
 module.exports = {
   mode: productionMode ? 'production' : 'development',
@@ -21,4 +33,5 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
+  optimization,
 }
